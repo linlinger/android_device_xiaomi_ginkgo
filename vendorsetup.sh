c85077ec34e0c5494ab8d04ddd2407a3c3f965ec
林凌儿 <32541118+linlinger@users.noreply.github.com>
2022-11-02 14:23:40 +0800

    Update vendorsetup.sh
    Remove auromaFM
    add support for willow

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,6 +37,31 @@
 	export FOX_USE_NANO_EDITOR=1
 	# 添加phhusson's lptools工具
 	export OF_ENABLE_LPTOOLS=1
+	# 移除 AromaFM
+	export FOX_DELETE_AROMAFM="1"
+	# 添加 备选设备
+	export TARGET_DEVICE_ALT="willow"
+
+	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+
+	export OF_FBE_METADATA_MOUNT_IGNORE=1
+	export OF_CHECK_OVERWRITE_ATTEMPTS=1
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND=1510672800
+	export OF_USE_TWRP_SAR_DETECT=1
+	export FOX_QUICK_BACKUP_LIST="/data;/boot;/vendor_image;/system_image"
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+	export OF_PATCH_AVB20=1
+	export FOX_REMOVE_AAPT=1
+	export FOX_ADVANCED_SECURITY=1
+
 
 	## 硬件功能设定
 
@@ -44,7 +69,7 @@
 	# 设置屏幕高度，状态栏高度，状态栏左右边距
 	# 由于橙狐默认屏幕比例是16：9,所以应设置屏幕高度为屏幕比例换算成n：9之后，n*120
 	export OF_SCREEN_H=2340
-	export OF_STATUS_H=59
+	export OF_STATUS_H=80
 	export OF_STATUS_INDENT_LEFT=48
 	export OF_STATUS_INDENT_RIGHT=48
 	# 禁止禁用导航栏
