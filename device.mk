71c66f615faaffbe4bbfc2a767c6b5f9e25b4275
linlinger <32541118+linlinger@users.noreply.github.com>
2023-10-18 16:53:34 +0800

    Rework DT

--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Api
-PRODUCT_SHIPPING_API_LEVEL := 28
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# qcom decryption
-PRODUCT_PACKAGES_ENG += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Custom ROM asserts
 TARGET_OTA_ASSERT_DEVICE := ginkgo,willow
@@ -12,6 +10,15 @@ TARGET_OTA_ASSERT_DEVICE := ginkgo,willow
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl.recovery
+
+# qcom decryption
+PRODUCT_PACKAGES_ENG += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
